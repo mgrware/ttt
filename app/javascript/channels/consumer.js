1eb95d152370969d1de8d46eb6d6dc8efe2411d8
Gilang Ramadan <mgrware@gmail.com>
2024-02-16 17:23:43 +0700

    remove hardcode

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -2,7 +2,7 @@
 // You can generate new channels where WebSocket features live using the `bin/rails generate channel` command.
 
 import { createConsumer } from "@rails/actioncable"
-const cableUrl  = "ws://localhost:3000/cable"
+const cableUrl  = "/cable"
 
 console.log(cableUrl)
 export default createConsumer(cableUrl)
